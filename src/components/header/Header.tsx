9bee9d0d64fe50a126f4349e8014f77771e110c1
G1anpierre <fgianpierre@gmail.com>
2024-02-16 17:14:17 +0100

    conclude navbar styling

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './Header.module.css'
+// import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import { Cart } from '../Icons/Cart'
+
+export const Header = () => {
+  return (
+    <header className={styles.container}>
+        <nav className={styles.navbar}>
+            <Link href='/gallery' className={styles.link}>
+                <div>Gallery</div>
+            </Link>
+        </nav>
+        <Link href="/" className={styles.link}>
+            <h1 className={styles.title}>Washazon</h1>
+        </Link>
+        <div className={styles.cart}>
+            <div>Cart</div>
+            <Cart height={20} width={20}/>
+        </div>
+    </header>
+  )
+}
