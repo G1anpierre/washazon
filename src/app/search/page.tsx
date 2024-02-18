5e7c39a1f968b6143a457578cc4845faf3df4a89
G1anpierre <fgianpierre@gmail.com>
2024-02-19 00:24:17 +0100

    chore: add container to gallery and search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,18 @@
 import { Products } from "@/components/products/Products";
 import React from "react";
 import { getSearchTerm } from "../api/search/search";
+import styles from "./SearchPage.module.css";
 
 const SearchPage = ({
   searchParams,
 }: {
   searchParams: { searchTerm: string };
 }) => {
-  return <Products fetchInfo={() => getSearchTerm(searchParams.searchTerm)} />;
+  return (
+    <section className={styles.container}>
+      <Products fetchInfo={() => getSearchTerm(searchParams.searchTerm)} />
+    </section>
+  );
 };
 
 export default SearchPage;
